7cb38465d7a2ff1d2a39915f59a268ff7120d96b
human <human@host.docker.internal>
2022-04-15 17:12:45 +0900

    will / change PaymentVO, PaymentDAO

--- src/com/team3/controller/PaymentServlet.java
+++ src/com/team3/controller/PaymentServlet.java
@@ -11,23 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Payment.do")
 public class PaymentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-
-    public PaymentServlet() {
-        super();
-        
-    }
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+	
 	}
 
 }
--- src/com/team3/dao/PaymentDAO.java
+++ src/com/team3/dao/PaymentDAO.java
@@ -1,10 +1,74 @@
 package com.team3.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-@WebServlet("/PaymentDAO")
-public class PaymentDAO extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import com.team3.dto.MemberVO;
+import com.team3.dto.PaymentVO;
+
+import util.DBManager;
+
+
+public class PaymentDAO  {
+	
+	private PaymentDAO() {		
+	}
+	
+	private static PaymentDAO instance = new PaymentDAO();
+	
+	public static PaymentDAO getInstance() {
+		return instance;
+	}
+	
+	
+	// Create (insert) - 회원 가입
+		public int insertPayment(PaymentVO rVo) {
+			String sql = "insert into payment values(payment_abs(rpad(dbms_random.random,4)),?,sysdate,?";
+			
+			int result = -1;
+			
+			Connection conn = null;
+			PreparedStatement pstmt= null;
+			
+			
+			try {
+				// 1. jdbc 드라이버 로드 : forName(className)
+				// 2. 디비 접속을 위한 연결 객체 생성 : getConnection(url, user, password)
+				conn = DBManager.getConnection();
+		
+				// 3. 쿼리문을 실행하기 위한 객체 생성
+//				stmt = conn.createStatement();
+				pstmt = conn.prepareStatement(sql);
+	
+				pstmt.setString(1, rVo.getPayment_type());
+				pstmt.setInt(2, rVo.getAmount());
+				// 4. 쿼리 실행 및 결과 처리
+				// executeUpdate(sql)	- insert update delete	
+				result = pstmt.executeUpdate();
+				
+			} catch(SQLException e) {
+				e.printStackTrace();
+			} finally {
+				DBManager.close(conn, pstmt);
+			}		
+			return result;		
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
