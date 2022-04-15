bb1d953808072d4d03955dd23926179c7ef0a68f
human <human@host.docker.internal>
2022-04-15 15:23:05 +0900

    will /
    plus paymentVO

--- src/com/team3/dto/PaymentVO.java
+++ src/com/team3/dto/PaymentVO.java
@@ -6,36 +6,50 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
-/**
- * Servlet implementation class PaymentVO
- */
-@WebServlet("/PaymentVO")
-public class PaymentVO extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PaymentVO() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+public class PaymentVO {
+
+	private int payment_number;
+	private String payment_type;
+	private Date payment_time;
+	private int amount;
+	
+	
+	public int getPayment_number() {
+		return payment_number;
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public void setPayment_number(int payment_number) {
+		this.payment_number = payment_number;
 	}
+	public String getPayment_type() {
+		return payment_type;
+	}
+	public void setPayment_type(String payment_type) {
+		this.payment_type = payment_type;
+	}
+	public Date getPayment_time() {
+		return payment_time;
+	}
+	public void setPayment_time(Date payment_time) {
+		this.payment_time = payment_time;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "PaymentVO [payment_number=" + payment_number + ", payment_type=" + payment_type + ", payment_time="
+				+ payment_time + ", amount=" + amount + "]";
+	}
+	
+	
+
 
 }
