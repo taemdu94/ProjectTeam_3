31b1f92dfe61d26f9c51354a133025f4e036221e
human <human@host.docker.internal>
2022-04-15 15:38:06 +0900

    will /
    plus PaymentDAO, ResersationDAO, PaymentDAO, PaymentServlet,
    ReservationServlet

---
+++ src/com/team3/dao/PaymentDAO.java
@@ -0,0 +1,10 @@
+package com.team3.dao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/PaymentDAO")
+public class PaymentDAO extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
